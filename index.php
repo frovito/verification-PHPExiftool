50139e48a7a57868a614d9b886439aad160fb1b2
Fernando Rovito <fer.rovito@gmail.com>
2023-04-05 15:33:44 +0200

    video test file added to test script

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
 <?php
 
-phpinfo();
+require __DIR__ . '/vendor/autoload.php';
 
+use Monolog\Logger;
+use PHPExiftool\Reader;
+use PHPExiftool\Driver\Value\ValueInterface;
+
+$logger = new Logger('exiftool');
+$reader = Reader::create($logger);
+
+$metadataBag = $reader->files("test-files/IMG_2259.mov")->first();
+
+foreach ($metadataBag as $metadata) {
+    if (ValueInterface::TYPE_BINARY === $metadata->getValue()->getType()) {
+        echo sprintf("\t--> Field %s has binary data" . PHP_EOL, $metadata->getTag());
+    } else {
+        echo sprintf("\t--> Field %s has value(s) %s" . PHP_EOL, $metadata->getTag(), $metadata->getValue()->asString());
+    }
+}
